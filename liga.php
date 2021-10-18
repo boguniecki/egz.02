7cff61026814dc9c0602773b5fe65399298757ed
boguniecki <marcin.boguniecki@gmail.com>
2021-10-18 14:47:25 +0200

    trzecia czesc zadania - poczatek

---
+++ liga.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>piłka nożna</title>
+    <link rel="stylesheet" href="styl2.css">
+</head>
+<body>
+
+    <header id="baner">
+        <h3>Reprezentacja polski w piłce nożnej</h3>
+        <img src="obraz1.jpg" alt="reprezentacja">
+    </header>
+    <section id="lewa"></section>
+    <section id="prawa"></section>
+    <main id="glowny"></main>
+    <section id="liga"></section>
+
+    
+</body>
+</html>
